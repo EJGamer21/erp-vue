3513794c7c03b99081a5bbc33d51475fe9dddbbb
Enger Jiménez <engersky12@gmail.com>
2019-09-09 15:07:13 -0400

    dates formatted

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const usersModel = require('../models/usersModel');
 const moment = require('moment');
 const api = new usersModel();
@@ -6,8 +8,23 @@ module.exports = {
     getAll: async (req, res) => {
         try {
             const users = await api.getAll();
+            users.forEach(user => {
+                if (moment(user.fecha_creacion).isValid()) {
+                    user.fecha_creacion = moment(user.fecha_creacion).format('YYYY-MM-DD HH:mm:ss');
+                } else {
+                    user.fecha_creacion = '';
+                }
+
+                if (moment(user.fecha_modificado).isValid()) {
+                    user.fecha_modificado = moment(user.fecha_modificado).format('YYYY-MM-DD HH:mm:ss');
+                } else {
+                    user.fecha_modificado = '';
+                }
+                
+            });
             res.json(users);
         } catch (error) {
+            console.log(error);
             res.status(500).send({
                 error: true,
                 message: 'Error 500: Internal server error',
@@ -19,8 +36,17 @@ module.exports = {
         const id = parseInt(req.params.id);
         try {
             const user = await api.getById(id);
-            user[0].fecha_creacion = moment(user[0].fecha_creacion).format('YYYY-MM-DD HH:mm:ss');
-            user[0].fecha_modificado = moment(user[0].fecha_modificado).format('YYYY-MM-DD HH:mm:ss');
+            if (moment(user[0].fecha_creacion).isValid()) {
+                user[0].fecha_creacion = moment(user[0].fecha_creacion).format('YYYY-MM-DD HH:mm:ss');
+            } else {
+                user[0].fecha_creacion = '';
+            }
+
+            if (moment(user[0].fecha_modificado).isValid()) {
+                user[0].fecha_modificado = moment(user[0].fecha_modificado).format('YYYY-MM-DD HH:mm:ss');
+            } else {
+                user[0].fecha_modificado = '';
+            }
             res.json(user);
         } catch (error) {
             console.log(error);
--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 // Requirements
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const routes = require('./routes');
 
 // App instance
+const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/', routes);
 
 // App started
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 app.listen(port, () => {
     console.log('Server listening on port: http://localhost:' + port)
 });

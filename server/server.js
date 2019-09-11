74d031c33774197517a377f56eca9d036fd8c075
Enger Jiménez <engersky12@gmail.com>
2019-09-11 15:58:00 -0400

    using cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+'use strict';
+
 // Requirements
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
@@ -8,11 +11,12 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
 app.use('/', routes);
 
 // App started
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
     console.log('Server listening on port: http://localhost:' + port)
 });

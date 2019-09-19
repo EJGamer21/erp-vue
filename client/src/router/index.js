23605f98a8c78b925eb0600693bfc191e8f849b9
Enger Jiménez <engersky12@gmail.com>
2019-09-19 15:57:17 -0400

    Home and Client route added

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,15 +2,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Users from '@/components/Users'
+import Home from '@/components/Home'
+import Clients from '@/components/Clients'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
       path: '/users',
-      name: 'Users',
+      name: 'users',
       component: Users
-    }
+    },
+    {
+      path: '/clients',
+      name: 'clients',
+      component: Clients
+    },
   ]
 })

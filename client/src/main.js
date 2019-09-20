2633c8d3a0af767ee403b95dee1413dbd7d5a8a7
Enger Jiménez <engersky12@gmail.com>
2019-09-20 15:38:25 -0400

    vue-session added

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-new */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import 'bootstrap/dist/css/bootstrap.css'
+import VueSession from 'vue-session'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '../style.css'
@@ -16,10 +17,10 @@ loadProgressBar()
 
 Vue.use(Toastr, toastConfigs)
 Vue.use(Buefy)
+Vue.use(VueSession, { persist: true })
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,

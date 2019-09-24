37a22b87ab3bbb96b9c65449fc2105bc13eaa7a9
Enger Jimenez <engersky12@gmail.com>
2019-09-24 16:35:13 -0400

    Direction route added to router

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,11 @@ const express = require('express');
 const router = express.Router();
 
 const userRoute = require('./users');
-// const clientRoute = require('./clients');
+const directionRoute = require('./directions');
+const clientRoute = require('./clients');
 
 router.use('/users', userRoute);
-// router.use('/clients', clientRoute);
+router.use('/directions', directionRoute);
+router.use('/clients', clientRoute);
 
 module.exports = router;

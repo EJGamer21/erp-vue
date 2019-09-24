1cf91448a3728d85ff25ffc6b346bb1e88475600
Enger Jimenez <engersky12@gmail.com>
2019-09-24 16:34:54 -0400

    Directions controller added

---
+++ server/controllers/directionsController.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const directionModel = require('../models/directionModel');
+const api = new directionModel();
+
+module.exports = {
+  getAll: async (req, res) => {
+    try {
+      const directions = await api.getAll();
+      res.json(directions);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+          error: true,
+          message: 'Error 500: Internal server error',
+      });
+    }
+  },
+  getById: async (req, res) => {
+      const id = parseInt(req.params.id);
+      try {
+          const user = await api.getById(id);
+          if (moment(user[0].fecha_creacion).isValid()) {
+              user[0].fecha_creacion = moment(user[0].fecha_creacion).format('YYYY-MM-DD HH:mm:ss');
+          } else {
+              user[0].fecha_creacion = '';
+          }
+
+          if (moment(user[0].fecha_modificado).isValid()) {
+              user[0].fecha_modificado = moment(user[0].fecha_modificado).format('YYYY-MM-DD HH:mm:ss');
+          } else {
+              user[0].fecha_modificado = '';
+          }
+          res.json(user);
+      } catch (error) {
+          console.log(error);
+          res.status(500).json({
+              error: true,
+              message: 'Error 500: Internal server error',
+          });
+      }
+  },
+}
